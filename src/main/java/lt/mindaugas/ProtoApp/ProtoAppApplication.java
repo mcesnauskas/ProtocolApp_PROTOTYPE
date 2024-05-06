ae2961106f22c47dfa78744cea2765017958a4c3
Mindaugas <mcesnauskas@gmail.com>
2024-05-06 17:46:59 +0300

    Refactor ProtoAppApplication class

--- src/main/java/lt/mindaugas/ProtoApp/ProtoAppApplication.java
+++ src/main/java/lt/mindaugas/ProtoApp/ProtoAppApplication.java
@@ -1,4 +1,4 @@
-package lt.mindaugas.ProtoApp.ProtoApp;
+package lt.mindaugas.ProtoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
