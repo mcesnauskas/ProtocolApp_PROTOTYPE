a615ea45014cedfd4c00ca9f3bae595f432cd6d0
Mindaugas <mcesnauskas@gmail.com>
2024-05-06 17:32:10 +0300

    Initial commit

---
+++ src/main/java/lt/mindaugas/ProtoApp/ProtoApp/ProtoAppApplication.java
@@ -0,0 +1,13 @@
+package lt.mindaugas.ProtoApp.ProtoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/mindaugas/ProtoApp/ProtoApp/ProtoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mindaugas.ProtoApp.ProtoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
