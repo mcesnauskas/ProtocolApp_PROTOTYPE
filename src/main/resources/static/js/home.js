250555139806630b6e40bf27dead30e61388f1e6
Mindaugas <mcesnauskas@gmail.com>
2024-05-14 00:12:22 +0300

    Redesign home.html and home.js

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,1 +1,20 @@
+document.getElementById('projectSelect').addEventListener('change', function() {
+        var selectedOption = this.options[this.selectedIndex];
+        var projectFullName = selectedOption.getAttribute('data-full-name');
+        var projectId = selectedOption.value;
+        document.getElementById('projectFullNameDisplay').textContent = projectFullName;
+        document.getElementById('addQuestionsButton').href = '/project/' + projectId + '/questions';
+        document.getElementById('addParticipantsButton').href = '/project/' + projectId + '/participants';
+    });
 
+    var buttons = document.querySelectorAll('#addQuestionsButton, #addParticipantsButton');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            var projectSelect = document.getElementById('projectSelect');
+            if (projectSelect.selectedIndex === -1 || projectSelect.value === '') {
+                e.preventDefault();
+                alert('Please select a project first.');
+            }
+        });
+    });
